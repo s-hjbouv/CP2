90d8e52fb000f1fc29ccfde0bbb6bcb28a02e2bb
Hjördis Bouvain <s-hjbouv@frontend.service>
2019-05-20 14:42:12 +0200

    kleinen Fehler in erster Zeile berichtigt

--- Multiply/main.cpp
+++ Multiply/main.cpp
@@ -3,12 +3,13 @@
 
 int main(int args, char** argv){
 std::ifstream in("input.txt"); // Liste mit Daten einlesen
-int var2;
-std::string var1;
-float a=1.8;
+
+int var2;             // Variablen definieren
+std::string var1; 
+float a=1.8; 
 float b=32;
 
-for(int i=0; i<5; i++){ // Loop basteln
+for(int i=0; i<8; i++){ // Loop basteln
 in >>var1;
 in >>var2;
 if (var1 == "F"){ // Unterscheidung, ob in Liste F oder C vorne steht
@@ -16,7 +17,7 @@ if (var1 == "F"){ // Unterscheidung, ob in Liste F oder C vorne steht
     std::cout << var2 << " Grad Celsius sind " << A << " Grad Fahrenheit " << std::endl;
 }
 else if (var1 == "C"){
-    double B = (var2-b)*a; // Fahrenheit in Celsius
+  double B = (var2-b)*a; // Fahrenheit in Celsius
     std::cout << var2 << " Grad Fahrenheit sind " << B << " Grad Celsius" << std::endl; 
 }
 else {
