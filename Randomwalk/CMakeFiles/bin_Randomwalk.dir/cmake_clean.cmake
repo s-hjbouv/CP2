54cff1b64769b8fd0f4923ae11d648b13dd69123
Hjördis Bouvain <s-hjbouv@frontend.service>
2019-06-26 12:11:59 +0200

    Randomwalk

---
+++ Randomwalk/CMakeFiles/bin_Randomwalk.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/bin_Randomwalk.dir/main.cpp.o"
+  "bin_Randomwalk.pdb"
+  "bin_Randomwalk"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/bin_Randomwalk.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ Randomwalk/main.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+#include <vector>
+#include <random>
+#include <fstream>
+
+
+int main(){
+
+ double x1;
+ double x2;
+ double x1a;
+
+ std::vector<int> Teilchenpositionen(10000000,0); //Teilchenzahl und ihre Positionen
+
+//Vektoren für die Zeitschritte, Wegschritte und x**2  
+  std::vector<double> Zeitschritt(10);
+  std::vector<double> Wegschritt(Teilchenpositionen.size());
+  std::vector<double> M1(10); //Vektor für erstes Moment
+  std::vector<double> M2(10); //Vektor für zweites Moment
+  std::vector<std::string> o(M1.size());
+
+std::mt19937 gen; //Zufallszahl definieren
+gen.seed(5); //Beginn Zufallszahlen
+
+// Schleife über Zeitschritte  
+for(int t =0; t< Zeitschritt.size(); t++) {
+    x1=0;
+    x2=0;
+    for(int i = 0; i < Teilchenpositionen.size(); i++) { //Schleife über Teilchenanzahlvektor
+      //Zufallszahlen
+      std::uniform_real_distribution<double> dis(0,1);
+     // std::cout <<"Zufallszahlen " <<dis(gen) << std::endl; // Zufallszahlen ausgeben
+     
+     //Schrittrichtung entscheiden
+      if(dis(gen) > 0.5){
+      Wegschritt[i] = Teilchenpositionen[i]++;
+      }
+      else{
+      Wegschritt[i] = Teilchenpositionen[i]--;
+      }
+     // Zeitschritt.push_back(t);  //neue Werte an Vektor hängen
+     // Wegschritt.push_back(Wegschritt[i]);  
+    }
+
+    // 1.Moment: Aufsummieren der Wegschritte pro Zeiteinheit
+    for (int m = 0; m < Wegschritt.size(); m++){
+      x1+= Wegschritt[m]; //Aufsummieren
+    }  
+    x1a = x1/Teilchenpositionen.size();
+    std::cout <<" 1. Moment: " << x1/Teilchenpositionen.size() <<std::endl;
+
+    // 2.Moment: Aufsummieren der Wegschritte zum Quadrat minus Mittelwert(1.Moment) pro Zeiteinheit
+    for (int m=0; m < Wegschritt.size(); m++){
+      x2+= (Wegschritt[m]-x1a)*(Wegschritt[m]-x1a);
+    }  
+    std::cout << "2. Moment: " << x2/Teilchenpositionen.size() << std::endl;
+    
+    M1.push_back(x2/Teilchenpositionen.size());
+    M2.push_back(x1/Teilchenpositionen.size());
+
+}
+
+// 1.Moment und 2.Moment pro Zeitschritt in Textdatei		//irgendwas ist da fehlerhaft
+std::ofstream output_file("output.txt");
+for (int i=0; i<M1.size(); i++ ){
+  o[i] = std::to_string(M1[i])+" "+std::to_string(M2[i]);
+  output_file << o[i] << std::endl;
+}
+
+ // D über Regression berechnen: 2D = 0.999682
+ // -- D = 0.499841
+ // 2.Moment = 2D*Zeitschritt
+
+
+
+
+//Vektoren als File ausgeben
+//std::ofstream output_Zeitschritt("./Zeitschritt.txt");
+//std::ofstream output_Wegschritt("./Wegschritt.txt");
+
+//Vektoren ausgeben
+//for(int n=0; n<Zeitschritt.size(); n++){
+//  std::cout << "Zeitschritt " << Zeitschritt[n] <<std::endl;
+//  std::cout <<"Wegschritt " <<Wegschritt[n] <<std::endl; 
+//}
+
+
+
+return 0;
+
+}
